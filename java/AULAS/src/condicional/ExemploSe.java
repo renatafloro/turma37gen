0a15ec6e53de4b88507b8cda14dd1fc319c27218
Tarcísio de Lima <32613596+tarcisio-lima@users.noreply.github.com>
2022-01-15 13:09:03 -0300

    Update ExemploSe.java
    Sugestão para separar as declarações dos arquivos

--- java/AULAS/src/condicional/ExemploSe.java
+++ java/AULAS/src/condicional/ExemploSe.java
@@ -7,10 +7,13 @@ public class ExemploSe {
 	public static void main(String[] args) {
 
 		Scanner leia = new Scanner(System.in);
+		
 		final int ANO_ATUAL = 2021;
-		String nome;
 		int anoNascimento;
 		int idade;
+		
+		String nome;
+		
 
 		System.out.println("Digite o nome: ");
 		nome = leia.next();
@@ -20,12 +23,12 @@ public class ExemploSe {
 		idade = ANO_ATUAL - anoNascimento;
 
 		if (idade >= 40) {
-			System.out.println("Você é cringe!");
+			System.out.println("VocÃª Ã© cringe!");
 		} else if (idade >= 18) {
-			System.out.println("Você é adulto!");
+			System.out.println("VocÃª Ã© adulto!");
 
 		} else {
-			System.out.println("Você é jovem!");
+			System.out.println("VocÃª Ã© jovem!");
 		}
 
 	}
